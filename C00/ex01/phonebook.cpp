61dab84f8bd80929eb837e8c338f512ced67614e
Mohammed Nassi <mnassi@e2r2p12.1337.ma>
2023-05-31 04:52:37 +0100

    42

--- C00/ex01/phonebook.cpp
+++ C00/ex01/phonebook.cpp
@@ -6,37 +6,220 @@
 /*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 05:19:15 by mnassi            #+#    #+#             */
-/*   Updated: 2023/05/30 05:27:09 by mnassi           ###   ########.fr       */
+/*   Updated: 2023/05/31 04:42:12 by mnassi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-class phonebook {
-	private:
-    	std::string	fname;
-    	std::string	lname;
-    	std::string	nname;
-    	int			phone;
-    	std::string	dark;
+void	ft_fill_1st(std::string& input)
+{
+	phonebook contact;
 
-	public:
-    	phonebook(const std::string& firstn, const std::string& lastn, const std::string& nickname, int number, const std::string& secret) {
-       		fname = firstn;
-        	lname = lastn;
-        	nname = nickname;
-        	phone = number;
-        	dark = secret;
-    	}
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact.setSecret(input);
+}
 
-	    std::string getfname() const {
-    	    return fname;
-    }
-};
+void	ft_fill_2st(std::string& input)
+{
+	phonebook contact2;
 
-int main() {
-    phonebook objectest("mohammed", "nassi", "mnassi", 618234576, "m9wda elia");
-    std::cout << objectest.getfname() << std::endl;
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact2.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact2.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact2.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact2.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact2.setSecret(input);
+}
 
-    return 0;
+void	ft_fill_3st(std::string& input)
+{
+	phonebook contact3;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact3.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact3.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact3.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact3.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact3.setSecret(input);
+}
+
+void	ft_fill_4st(std::string& input)
+{
+	phonebook contact4;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact4.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact4.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact4.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact4.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact4.setSecret(input);
+}
+
+void	ft_fill_5st(std::string& input)
+{
+	phonebook contact5;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact5.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact5.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact5.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact5.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact5.setSecret(input);
+}
+
+void	ft_fill_6st(std::string& input)
+{
+	phonebook contact6;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact6.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact6.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact6.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact6.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact6.setSecret(input);
+}
+
+void	ft_fill_7st(std::string& input)
+{
+	phonebook contact7;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact7.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact7.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact7.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact7.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact7.setSecret(input);
+}
+
+void	ft_fill_8st(std::string& input)
+{
+	phonebook contact8;
+
+	std::cout << "Enter first name : ";
+	std::cin >> input;
+	contact8.setName(input);
+	std::cout << "Enter sec name : ";
+	std::cin >> input;
+	contact8.setLName(input);
+	std::cout << "Enter nickname : ";
+	std::cin >> input;
+	contact8.setNName(input);
+	std::cout << "Enter phone number : ";
+	std::cin >> input;
+	contact8.setPNumber(input);
+	std::cout << "Enter a secret : ";
+	std::cin >> input;
+	contact8.setSecret(input);
+}
+
+int	askinguser(std::string& input, int nmb)
+{
+	phonebook contact;
+
+	nmb = contact.incrementcontact(nmb);
+	if (nmb == 1)
+		ft_fill_1st(input);
+	if (nmb == 2)
+		ft_fill_2st(input);
+	if (nmb == 3)
+		ft_fill_3st(input);
+	if (nmb == 4)
+		ft_fill_4st(input);
+	if (nmb == 5)
+		ft_fill_5st(input);
+	if (nmb == 6)
+		ft_fill_6st(input);
+	if (nmb == 7)
+		ft_fill_7st(input);
+	if (nmb == 8)
+		ft_fill_8st(input);
+	return (nmb);
+}
+
+int main()
+{
+	phonebook inx;
+	int			i;
+	std::string	input;
+
+	i = 0;
+	while (input != "EXIT")
+	{
+		inx.incrementcontact(i);
+		std::cout << "Enter a command : ";
+		std::cin >> input;
+		if (input == "ADD")
+			i = askinguser(input, i);
+		else if (input == "SEARCH")
+			ft_search()
+	}
+	return (0);
 }
--- C00/ex01/phonebook.hpp
+++ C00/ex01/phonebook.hpp
@@ -6,14 +6,64 @@
 /*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 05:24:45 by mnassi            #+#    #+#             */
-/*   Updated: 2023/05/30 05:25:33 by mnassi           ###   ########.fr       */
+/*   Updated: 2023/05/31 04:18:16 by mnassi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+class phonebook {
+	private:
+    	std::string	fname;
+    	std::string	lname;
+    	std::string	nname;
+    	std::string	phone;
+    	std::string	dark;
+
+	public:
+    	void constr(const std::string& firstn, const std::string& lastn, const std::string& nickname, std::string& number, const std::string& secret)
+		{
+       		fname = firstn;
+        	lname = lastn;
+        	nname = nickname;
+        	phone = number;
+        	dark = secret;
+    	}
+		void setName(const std::string& frst){
+			if (!frst.empty())
+				fname = frst;
+		}
+		void setLName(const std::string& sec){
+			if (!sec.empty())
+				lname = sec;
+		}
+		void setNName(const std::string& nick){
+			if (!nick.empty())
+				nname = nick;
+		}
+		void setPNumber(const std::string& number){
+			if (!number.empty())
+				phone = number;
+		}
+		void setSecret(const std::string& secr){
+			if (!secr.empty())
+				dark = secr;
+		}
+		int	incrementcontact(int i)
+		{
+			if (i <= 8)
+				i++;
+			return (i);
+		}
+	    std::string getfname() const {
+    	    return fname;
+    }
+};
 
 #endif
