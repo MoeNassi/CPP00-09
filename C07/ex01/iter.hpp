0ed3221aa9f9fd5e8c5e0f436563eabcb7cad474
medaitswa <medaitswa@gmail.com>
2023-09-25 15:11:17 +0200

    42

---
+++ C07/ex01/iter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 14:45:26 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/25 14:45:26 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template<typename T, typename Func>
+void iter( T *array, size_t size, Func function) {
+	for (size_t i = 0; i < size ; i++)
+		function(array[i]);
+}
+
+template<typename T>
+void printarray( const T &nb_) {
+	std::cout << nb_ << std::endl;
+}
+
+#endif
