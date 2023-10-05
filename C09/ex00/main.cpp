0bb77dd534caab5d8d74762f7792900361ef3465
Mohammed Nassi <mnassi@e3r10p6.1337.ma>
2023-10-05 19:03:53 +0100

    42

--- C09/ex00/main.cpp
+++ C09/ex00/main.cpp
@@ -19,44 +19,80 @@ bool	checkfile( st_ file ) {
 	return (false);
 }
 
-bool	main_ft(st_ input) { // check after |
-	int	pip = 0;
-	int	minus = 0;
-	st_ check = "YYYY-MM-DD ";
-	for (int i = 0; input[i]; i++) {
-		check[i] = input[i];
-		if (input[i] == '-')
-			minus++;
-		if (input[i] == '|')
-			pip++;
-	}
-	if (input == check)
-		return true;
-	if (minus != 2 || pip != 1)
-		return (false);
-	return false;
+bool	check_if_nb( st_ type ) {
+	for (int i = 0; type[i] && type[i] != ' '; i++)
+		if (!isdigit(type[i]))
+			return (false);
+	return (true);
 }
 
-bool	date_value( st_ input ) {
-	if (!main_ft(input))
+bool	linear( st_ year, st_ month, st_ day ) {
+	if (!check_if_nb(year) || !check_if_nb(day) || !check_if_nb(month))
+		return (false);
+	if (year.length() == 4 && month.length() == 2 && day.length() == 3)
+		return (true);
+	return (false);
+}
+
+bool	four_two_two( st_ file ) {
+	std::fstream	check(file);
+	if (!check.is_open())
+		return (false);
+	st_ year;
+	st_ month;
+	st_ day;
+	st_	stock;
+	std::getline(check, year, '-');
+	std::getline(check, month, '-');
+	std::getline(check, day, '|');
+	if (!linear( year, month, day ))
+		return (false);
+	return (true);
+}
+
+bool	main_ft(st_ input, st_ file) { // check for max days
+	int	minus = 0;
+	for (int i = 0; input[i]; i++) {
+		if (input[i] == '-')
+			minus++;
+	}
+	if (!four_two_two( file ))
+		return false;
+	if (minus != 2)
+		return (false);
+	return true;
+}
+
+bool	ft_value( st_ input ) {
+	for (int i = 0; input[i]; i++)
+		if (isdigit(input[i]))
+			return (true);
+	return (false);
+}
+
+bool	date_value( st_ input, st_ file ) {
+	if (input[0] == ' ' && isdigit(input[1]))
+		ft_value( &input[1] );
+	else if (main_ft(input, file))
+		return (true);
+	else
 		return (false);
 	return (true);
 }
 
 bool	then_checkThis( st_ file ) {
-	(void)file;
-	std::ifstream	in("input.txt");
+	std::ifstream	in(file);
 	st_				input;
 	if (!in.is_open())
 		return (false);
 	while (std::getline(in, input, '|'))
-		if (!date_value(input))
+		if (!date_value(input, file))
 			return (false);
 	return (true);
 }
 
 int main(int ac, char **av) {
-	if (ac >= 3)
+	if (ac > 2)
 		return (std::cout << "Too Many Arguments" << std::endl, 0);
 	if (!checkfile(av[1]))
 		return (std::cout << "Extension Error" << std::endl, 0);
